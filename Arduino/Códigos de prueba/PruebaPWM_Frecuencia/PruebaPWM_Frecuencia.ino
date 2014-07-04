e2582a500fe6534c4f8f8e78920e8d08071c5019
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2014-07-04 10:15:26 -0430

    04/07/2014 10:15am

--- Arduino/Códigos de prueba/PruebaPWM_Frecuencia/PruebaPWM_Frecuencia.ino
+++ Arduino/Códigos de prueba/PruebaPWM_Frecuencia/PruebaPWM_Frecuencia.ino
@@ -1,12 +1,14 @@
 void setup()
 {
   pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
   bitSet(TCCR1B, WGM12);
-  bitSet(TCCR2B, WGM12);
+  //bitSet(TCCR2B, WGM12);
+  digitalWrite(13, HIGH);
 }
 
 
 void loop()
 {
-  analogWrite(9, 150);
+  analogWrite(9, 250);
 }
