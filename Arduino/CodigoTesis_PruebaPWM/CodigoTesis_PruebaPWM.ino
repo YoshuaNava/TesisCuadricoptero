49cba2001bc794c82e27426caac27bbe618093b0
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2014-07-05 18:05:58 -0430

    05/07/2014 06:05pm
    Arduino, PWM.

--- Arduino/CodigoTesis_PruebaPWM/CodigoTesis_PruebaPWM.ino
+++ Arduino/CodigoTesis_PruebaPWM/CodigoTesis_PruebaPWM.ino
@@ -144,8 +144,11 @@ int correccionAltura = 0;
   pinMode(PUERTOMOTORINFERIOR, OUTPUT);
 
   // Modo FAST PWM en los pines 9, 10 y 11 //
-  bitSet(TCCR1B, WGM12);
-  bitSet(TCCR2B, WGM12);
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22);
+  
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM12);
   ///////////////////////////////////////////
   
   tiempo = micros();
--- Arduino/Códigos de prueba/PruebaPWM_Frecuencia/PruebaPWM_Frecuencia.ino
+++ Arduino/Códigos de prueba/PruebaPWM_Frecuencia/PruebaPWM_Frecuencia.ino
@@ -1,19 +1,43 @@
-void setup()
-{
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
   pinMode(9, OUTPUT);
-  pinMode(13, OUTPUT);
-  bitSet(TCCR1B, WGM12);
-  //bitSet(TCCR2B, WGM12);
-  digitalWrite(13, HIGH);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  Serial.begin(115200);
+
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22);
+  
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM12);
+  //  bitSet(TCCR2B, WGM22);
 }
 
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(5,120);
+  analogWrite(9,120);
+  analogWrite(10,120);
+  analogWrite(11,120);
+  delay(1000);
 
-void loop()
-{
-  int i;
-  for(i=0; i<255 ;i++)
-  {
-    analogWrite(9, 50);
-    delay(5);
-  }
+  analogWrite(5,0);
+  analogWrite(9,0);
+  analogWrite(10,0);
+  analogWrite(11,0);
+  delay(2000);
+
+//  Serial.println("Registros");
+//  Serial.println("TCCR1A");
+//  Serial.println(TCCR1A, BIN);
+//  Serial.println("TCCR1B");
+//  Serial.println(TCCR1B, BIN);
+//  Serial.println("TCCR2A");
+//  Serial.println(TCCR2A, BIN);
+//  Serial.println("TCCR2B");
+//  Serial.println(TCCR2B, BIN);  
+//  Serial.println();
+//  delay(3000);
 }
