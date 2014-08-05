aa632094d662a46d504e51fb682781a7280ccfda
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2014-08-05 10:27:58 -0430

    Circuitos de lógica y control de motores

--- Circuitos/Circuito de Motores y Alimentación/CircuitoMA_Tesis/Prototipo_2X_DobleCapa.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"pvengine.exe" +W320 +H240 +FN +L"c:/windows/fonts" +L"C:/Program Files (x86)/eagle3d/ulp//../povray" +O"C:/Users/Yoshua/Documents/GitHub/TesisCuadricoptero/Circuitos/Prototipo_2X_DobleCapa.png" /RENDER "C:/Users/Yoshua/Documents/GitHub/TesisCuadricoptero/Circuitos/Prototipo_2X_DobleCapa.pov" 
