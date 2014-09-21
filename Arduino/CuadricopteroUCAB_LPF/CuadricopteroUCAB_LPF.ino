f433fc7c001fdfbdfe36cc5dd7a9e3c81a1ab36f
Yoshua_ROSIndigo <yoshua.nava.chocron@gmail.com>
2014-09-21 14:54:09 -0430

    Arduino: Filtro pasa-bajos de velocidad angular.

--- Arduino/CuadricopteroUCAB_LPF/CuadricopteroUCAB_LPF.ino
+++ Arduino/CuadricopteroUCAB_LPF/CuadricopteroUCAB_LPF.ino
@@ -4,44 +4,49 @@
 #include <PID_v1.h>
 #include <NewPing.h>
 
-//CONSTANTES:
-//IMPORTANTE!!: ARREGLAR CON LOS PUERTOS QUE VAMOS A CONECTAR EN EL ARDUINO
-
-//ULTRASONIDO:
-#define USPIN 15 //puerto de datos del ultradonido.
-#define ALTURA_MAXIMA 150
-#define INCREMENTO_ALTURA_COMANDO 5
-NewPing sonar(USPIN, USPIN, ALTURA_MAXIMA);
-unsigned int uS;
-double alturaDeseada = 0;
-double distancia = 0;
-double USAltura = 0; // Distancia medida por el sensor de ultrasonido
-double correccionAltura = 0;
-//FIN ULTRASONIDO
-
-
+//CONSTANTES y VARIABLES:
 //MOTORES:
 #define PUERTOMOTORDERECHO 5 //puerto de PWM del motor derecho
 #define PUERTOMOTORIZQUIERDO 9 //puerto de PWM del motor izquierdo
 #define PUERTOMOTORINFERIOR 10 //puerto de PWM del motor inferior
 #define PUERTOMOTORSUPERIOR 11 //puerto de PWM del motor superior
 #define PWM_MAXIMO 255 //maximo PWM que puede enviar el arduino a los motores
-int velocidadBasePWM = 120;
+int velocidadBasePWM = 180;
 char modoEjecucion = '_';
 int motorDerecho = 0;
 int motorIzquierdo = 0;
-int motorDelantero = 0;
+int motorDelantero = 0; 
 int motorTrasero = 0;
 //FIN MOTORES
 
 
+//ULTRASONIDO:
+#define USPIN 15 //puerto de datos del ultradonido.
+#define ALTURA_MAXIMA 150
+#define INCREMENTO_ALTURA_COMANDO 5 ///////////////////////////// CAMBIAR A 5????
+NewPing sonar(USPIN, USPIN, ALTURA_MAXIMA);
+unsigned int uS;
+double alturaDeseada = 0;
+double distancia = 0;
+double USAltura = 0; // Distancia medida por el sensor de ultrasonido
+double correccionAltura = 0;
+double covarianzaProcesoFisicoAltura = 0.1;
+double covarianzaRuidoSensorAltura = 10.0;
+double estimacionAltura = 0.0;
+double covarianzaRuidoEstimacionAltura = 3.0;
+double gananciaKalman = 0.0;
+//FIN ULTRASONIDO
+
+
 //CODIGOS DE COMUNICACION:
+#define DT_envioDatos 50
 #define LED_ENCENDIDO 13
 #define CODIGO_INICIO_MENSAJE 255
 #define CODIGO_ENCENDIDO 0
 #define CODIGO_MOVIMIENTO 1
 #define CODIGO_ACK 6
 #define CODIGO_ESTADO 7
+#define MAXIMO_ANGULO_COMANDO 30
 unsigned char headerMensaje;
 unsigned char codigoRecibido;
 unsigned char comandoEncendidoRecibido;
@@ -59,18 +64,20 @@ unsigned char ack[4];
 #define ToDeg(x) ((x)*57.2957795131)  // *180/pi
 #define G_GYRO 0.00875
 #define G_ACC 0.0573
-#define K_COMP 0.93
-#define DT_envioDatos 100
+#define K_COMP 0.92
 #define DT_sensor_altura 29
 #define DT_PID_altura 50
-#define DT_PID_posicionAngular 1
-#define DT_PID_velocidadAngular 1
+#define DT_PID_posicionAngular 15
+#define DT_PID_velocidadAngular 5
 
 L3G gyro;
 LSM303 compass;
 char report[80];
 double yaw_offset = 0;
 double anguloDeseadoYPR[3] = {
+  0, 0, 0
+};
+double G_lecturaVelocidadYPR[3] = {
   0, 0, 0
 };
 double G_velocidadYPR[3] = {
@@ -95,20 +102,25 @@ double correccionPWM_YPR[3] = {
   0, 0, 0
 };
 double DT = 0;
+double alpha_LPF_velocidad = 0.98;
 //FIN IMU
 
-//TIEMPO
+//VARIABLES GLOBALES:
 long tiempoUltimoMuestreoAngulos = 0;
 long tiempoUltimoMuestreoAltura = 0;
 long tiempoUltimoEnvio = 0;
-//FIN TIEMPO
 
-//PID
+
+
+
+
 PID PID_vAngular_Yaw(&G_velocidadYPR[0], &correccionPWM_YPR[0], &velocidadDeseadaYPR[0], 0, 0, 0, DIRECT);
-PID PID_pAngular_Pitch(&anguloYPR[1], &correccionPWM_YPR[1], &anguloDeseadoYPR[1], 0, 0, 0, DIRECT);
-PID PID_pAngular_Roll(&anguloYPR[2], &correccionPWM_YPR[2], &anguloDeseadoYPR[2], 0, 0, 0, DIRECT);
-PID PID_altura(&USAltura, &correccionAltura, &alturaDeseada, 0, 0, 0, DIRECT);
-//FIN PID
+PID PID_vAngular_Pitch(&G_velocidadYPR[1], &correccionPWM_YPR[1], &velocidadDeseadaYPR[1], 0, 0, 0, DIRECT);
+PID PID_vAngular_Roll(&G_velocidadYPR[2], &correccionPWM_YPR[2], &velocidadDeseadaYPR[2], 0, 0, 0, DIRECT);
+PID PID_pAngular_Yaw(&anguloYPR[0], &velocidadDeseadaYPR[0], &anguloDeseadoYPR[0], 0, 0, 0, DIRECT);
+PID PID_pAngular_Pitch(&anguloYPR[1], &velocidadDeseadaYPR[1], &anguloDeseadoYPR[1], 0, 0, 0, DIRECT);
+PID PID_pAngular_Roll(&anguloYPR[2], &velocidadDeseadaYPR[2], &anguloDeseadoYPR[2], 0, 0, 0, REVERSE);
+PID PID_altura(&estimacionAltura, &correccionAltura, &alturaDeseada, 0, 0, 0, DIRECT);
 
 
 void setup() {
@@ -151,22 +163,28 @@ void setup() {
 
 
   // Parametros de los Algoritmos PID //
-
+  PID_pAngular_Yaw.SetSampleTime(DT_PID_posicionAngular);
   PID_pAngular_Pitch.SetSampleTime(DT_PID_posicionAngular);
   PID_pAngular_Roll.SetSampleTime(DT_PID_posicionAngular);
   PID_vAngular_Yaw.SetSampleTime(DT_PID_velocidadAngular);
+  PID_vAngular_Pitch.SetSampleTime(DT_PID_velocidadAngular);
+  PID_vAngular_Roll.SetSampleTime(DT_PID_velocidadAngular);
   PID_altura.SetSampleTime(DT_PID_altura);
 
-
+  PID_pAngular_Yaw.SetOutputLimits(-180.0, 180.0);
   PID_pAngular_Pitch.SetOutputLimits(-180.0, 180.0);
   PID_pAngular_Roll.SetOutputLimits(-180.0, 180.0);
   PID_vAngular_Yaw.SetOutputLimits(-PWM_MAXIMO, PWM_MAXIMO);
+  PID_vAngular_Pitch.SetOutputLimits(-PWM_MAXIMO, PWM_MAXIMO);
+  PID_vAngular_Roll.SetOutputLimits(-PWM_MAXIMO, PWM_MAXIMO);
   PID_altura.SetOutputLimits(-PWM_MAXIMO, PWM_MAXIMO);
 
-
+  PID_pAngular_Yaw.SetMode(AUTOMATIC);
   PID_pAngular_Pitch.SetMode(AUTOMATIC);
   PID_pAngular_Roll.SetMode(AUTOMATIC);
   PID_vAngular_Yaw.SetMode(AUTOMATIC);
+  PID_vAngular_Pitch.SetMode(AUTOMATIC);
+  PID_vAngular_Roll.SetMode(AUTOMATIC);
   PID_altura.SetMode(AUTOMATIC);
   //////////////////////////////////////
 
@@ -180,16 +198,23 @@ void setup() {
 
 void loop()
 {
-  PID_pAngular_Pitch.SetTunings(0.4, 0, 0);
-  PID_pAngular_Roll.SetTunings(0.4, 0, 0);
-  PID_vAngular_Yaw.SetTunings(0.4, 0, 0);
-  PID_altura.SetTunings(0, 0, 0);
-  alturaDeseada = 30;
+
+  // Yaw-  P: 1    I: 0   D: 0
+  PID_pAngular_Yaw.SetTunings(0, 0, 0);
+  PID_pAngular_Pitch.SetTunings(1.7, 0, 0);
+  PID_pAngular_Roll.SetTunings(1.7, 0, 0);
+
+  // Yaw-  P: 1.3  I: 0    D: 0
+  PID_vAngular_Yaw.SetTunings(0.4, 0, 0.002);
+  PID_vAngular_Pitch.SetTunings(0.65, 0, 0.01); //P=0.75   //P=0.55
+  PID_vAngular_Roll.SetTunings(0.65, 0, 0.01); //P=0.75   //P=0.55
+  PID_altura.SetTunings(2.0, 0, 0);
+
   //  PID_altura.SetTunings(1, 0, 0);
 
   modoEjecucion = '_';
-  //RecibirComando();
-  RecibirComandoASCII();
+  RecibirComando();
+  //RecibirComandoASCII();
   SecuenciaDeInicio();
   SecuenciaDeVuelo();
 }
@@ -203,18 +228,20 @@ void SecuenciaDeInicio()
     FiltroComplementario();
     CalcularAltura();
     EnviarMensajeEstado();
-    //RecibirComando();
-    RecibirComandoASCII();
+    RecibirComando();
+    //RecibirComandoASCII();
+    anguloDeseadoYPR[1] = 0;
+    anguloDeseadoYPR[2] = 0;
+    alturaDeseada = 0;
     i++;
   }
+
+  alturaDeseada = estimacionAltura;
 
 
   i = 0;
   if (modoEjecucion != '_')
   {
-    anguloDeseadoYPR[1] = 0;
-    anguloDeseadoYPR[2] = 0;
-    alturaDeseada = 0;
     while (i < velocidadBasePWM / 2)
     {
       if (modoEjecucion == '_')
@@ -228,18 +255,22 @@ void SecuenciaDeInicio()
       {
         motorDerecho = i;
         motorIzquierdo = i;
-        motorDelantero = i;
+        motorDelantero = i;  
         motorTrasero = i;
-        SistemasDeControl();
+        PIDAltura();
+        PID_VelocidadAngular();
         AplicarPWMmotores(i);
       }
       FiltroComplementario();
       CalcularAltura();
+      PID_PosicionAngular();
+      PID_VelocidadAngular();
+      AplicarPWMmotores(velocidadBasePWM);
       EnviarMensajeEstado();
-      //RecibirComando();
-      RecibirComandoASCII();
+      RecibirComando();
+      //RecibirComandoASCII();
       i++;
-      delay(5);
+      delay(1);
     }
   }
   else
@@ -256,21 +287,23 @@ void SecuenciaDeVuelo()
 {
   while (modoEjecucion != '_')
   {
-    //RecibirComando();
-    RecibirComandoASCII();
+    RecibirComando();
+    //RecibirComandoASCII();
     FiltroComplementario();
     CalcularAltura();
-    SistemasDeControl();
+    PIDAltura();
+    PID_PosicionAngular();
+    PID_VelocidadAngular();
     AplicarPWMmotores(velocidadBasePWM);
-/*    Serial.println("*************** Motores *****************");
-    Serial.println(motorDerecho);
-    Serial.println(motorIzquierdo);
-    Serial.println(motorDelantero);    
-    Serial.println(motorTrasero);
-    Serial.println(correccionPWM_YPR[0]);
-    Serial.println(correccionPWM_YPR[1]);
-    Serial.println(correccionPWM_YPR[2]);    
-    Serial.println();*/
+    /*    Serial.println("*************** Motores *****************");
+     Serial.println(motorDerecho);
+     Serial.println(motorIzquierdo);
+     Serial.println(motorDelantero);    
+     Serial.println(motorTrasero);
+     Serial.println(correccionPWM_YPR[0]);
+     Serial.println(correccionPWM_YPR[1]);
+     Serial.println(correccionPWM_YPR[2]);    
+     Serial.println();*/
     EnviarMensajeEstado();    
   }
 }
@@ -283,10 +316,13 @@ void FiltroComplementario() {
 
   DT = (double)(micros() - tiempoUltimoMuestreoAngulos) / 1000000;
 
-  G_velocidadYPR[0] = (double) (gyro.g.z * G_GYRO - yaw_offset);
-  G_velocidadYPR[1] = (double) gyro.g.x * G_GYRO;
-  G_velocidadYPR[2] = (double) gyro.g.y * G_GYRO;
+  G_lecturaVelocidadYPR[0] = (double) (gyro.g.z * G_GYRO - yaw_offset);
+  G_lecturaVelocidadYPR[1] = (double) gyro.g.x * G_GYRO;
+  G_lecturaVelocidadYPR[2] = (double) gyro.g.y * G_GYRO;
 
+  G_velocidadYPR[0] = G_velocidadYPR[0] * alpha_LPF_velocidad + (1 - alpha_LPF_velocidad)*G_lecturaVelocidadYPR[0];
+  G_velocidadYPR[1] = G_velocidadYPR[1] * alpha_LPF_velocidad + (1 - alpha_LPF_velocidad)*G_lecturaVelocidadYPR[1];
+  G_velocidadYPR[2] = G_velocidadYPR[2] * alpha_LPF_velocidad + (1 - alpha_LPF_velocidad)*G_lecturaVelocidadYPR[2];
 
   A_aceleracionYPR[0] = (double) compass.a.z * G_ACC;
   A_aceleracionYPR[1] = (double) compass.a.y * G_ACC;
@@ -329,7 +365,8 @@ void CalcularAltura()
     if ((distancia > 0) && (distancia < ALTURA_MAXIMA))
     {
       USAltura = distancia;
-      mensajeEstado[8] = USAltura;
+      FiltroKalmanAltura();
+      mensajeEstado[8] = estimacionAltura;
       //    Serial.print(USAltura);
       //    Serial.println("cm");
     }
@@ -338,12 +375,32 @@ void CalcularAltura()
 }
 
 
-void SistemasDeControl()
+void FiltroKalmanAltura()
 {
-  PID_vAngular_Yaw.Compute();
+  covarianzaRuidoEstimacionAltura = covarianzaRuidoEstimacionAltura + covarianzaProcesoFisicoAltura;
+  gananciaKalman = covarianzaRuidoEstimacionAltura/(covarianzaRuidoEstimacionAltura + covarianzaRuidoSensorAltura);
+  estimacionAltura = estimacionAltura + gananciaKalman * (USAltura - estimacionAltura);
+  covarianzaRuidoEstimacionAltura = (1 - gananciaKalman) * covarianzaRuidoEstimacionAltura;
+}
+
+
+void PID_PosicionAngular()
+{
+  PID_pAngular_Yaw.Compute();
   PID_pAngular_Pitch.Compute();
   PID_pAngular_Roll.Compute();
-  PID_altura.Compute();  
+}
+
+void PID_VelocidadAngular()
+{
+  PID_vAngular_Yaw.Compute();
+  PID_vAngular_Pitch.Compute();
+  PID_vAngular_Roll.Compute();
+}
+
+void PIDAltura()
+{
+  PID_altura.Compute();
 }
 
 
@@ -359,8 +416,8 @@ void AplicarPWMmotores(int velocidadMotoresPWM)
   }
   if (modoEjecucion == 'T')
   {
-    motorDerecho = velocidadMotoresPWM - correccionPWM_YPR[2] + correccionPWM_YPR[0] + correccionAltura;
-    motorIzquierdo = velocidadMotoresPWM + correccionPWM_YPR[2] + correccionPWM_YPR[0] + correccionAltura;
+    motorDerecho = velocidadMotoresPWM + correccionPWM_YPR[2] + correccionPWM_YPR[0] + correccionAltura;
+    motorIzquierdo = velocidadMotoresPWM - correccionPWM_YPR[2] + correccionPWM_YPR[0] + correccionAltura;
     motorDelantero = velocidadMotoresPWM - correccionPWM_YPR[1] - correccionPWM_YPR[0] + correccionAltura;
     motorTrasero = velocidadMotoresPWM + correccionPWM_YPR[1] - correccionPWM_YPR[0] + correccionAltura;
   }
@@ -409,8 +466,9 @@ void AplicarPWMmotores(int velocidadMotoresPWM)
 
 
 
-
 ///////////////////COMUNICACION/////////////////////////////////////////
+
+
 
 /*Procedimiento para enviar el estado del cuadricoptero
  Envia un paquete de 14 bytes
@@ -432,7 +490,7 @@ void AplicarPWMmotores(int velocidadMotoresPWM)
 void PrepararPaqueteMensajeEstado()
 {
   /**POSICION YAW**/
-  if (anguloYPR[0] >= 0)
+  if(anguloYPR[0] >= 0)
   {
     mensajeEstado[2] = anguloYPR[0];
     mensajeEstado[3] = 0;
@@ -440,14 +498,14 @@ void PrepararPaqueteMensajeEstado()
   else
   {
     mensajeEstado[3] = abs(anguloYPR[0]);
-    mensajeEstado[2] = 0;
+    mensajeEstado[2] = 0;    
   }
   /**POSICION PICH**/
   mensajeEstado[4] = anguloYPR[1] + 90;
   /**POSICION ROLL**/
   mensajeEstado[5] = anguloYPR[2] + 90;
   /**VELOCIDAD YAW**/
-  if (G_velocidadYPR[0] >= 0)
+  if(G_velocidadYPR[0] >= 0)
   {
     mensajeEstado[6] = G_velocidadYPR[0];
     mensajeEstado[7] = 0;
@@ -458,7 +516,7 @@ void PrepararPaqueteMensajeEstado()
     mensajeEstado[6] = 0;
   }
   /**VELOCIDAD PITCH**/
-  if (G_velocidadYPR[1] >= 0)
+  if(G_velocidadYPR[1] >= 0)
   {
     mensajeEstado[8] = G_velocidadYPR[1];
     mensajeEstado[9] = 0;
@@ -469,7 +527,7 @@ void PrepararPaqueteMensajeEstado()
     mensajeEstado[8] = 0;
   }
   /**VELOCIDAD ROLL**/
-  if (G_velocidadYPR[2] >= 0)
+  if(G_velocidadYPR[2] >= 0)
   {
     mensajeEstado[10] = G_velocidadYPR[2];
     mensajeEstado[11] = 0;
@@ -480,7 +538,8 @@ void PrepararPaqueteMensajeEstado()
     mensajeEstado[10] = 0;
   }
 
-  mensajeEstado[12] = USAltura;
+  //  mensajeEstado[12] = estimacionltura;
+  mensajeEstado[12] = alturaDeseada;
 }
 
 void EnviarMensajeEstado()
@@ -489,13 +548,16 @@ void EnviarMensajeEstado()
   {
     //ImprimirEstado();
     PrepararPaqueteMensajeEstado();
-    mensajeEstado[0] = CODIGO_INICIO_MENSAJE; //HEADER
+    mensajeEstado[0]= CODIGO_INICIO_MENSAJE;//HEADER
     mensajeEstado[1] = CODIGO_ESTADO; //Codigo del mensaje
-    mensajeEstado[13] = (mensajeEstado[0] ^  mensajeEstado[1] ^   mensajeEstado[2] ^ mensajeEstado[3] ^ mensajeEstado[4] ^ mensajeEstado[5] ^ mensajeEstado[6] ^ mensajeEstado[7] ^ mensajeEstado[8] ^ mensajeEstado[9] ^ mensajeEstado[10] ^ mensajeEstado[11] ^ mensajeEstado[12]); //CHECKSUM
+    mensajeEstado[13]=(mensajeEstado[0] ^  mensajeEstado[1] ^   mensajeEstado[2] ^ mensajeEstado[3] ^ mensajeEstado[4] ^ mensajeEstado[5] ^ mensajeEstado[6] ^ mensajeEstado[7] ^ mensajeEstado[8] ^ mensajeEstado[9] ^ mensajeEstado[10] ^ mensajeEstado[11] ^ mensajeEstado[12]);//CHECKSUM 
     Serial.write(mensajeEstado, 14);//ENVIAR EL PAQUETE DE 14 BYTES
     tiempoUltimoEnvio = millis();
   }
 }
+
+
+/*recepcion de mensajes*/
 
 void RecibirComando()
 {
@@ -521,14 +583,14 @@ void RecibirComando()
               {
                 checksum = Serial.read();
                 delay(1);
-                if (CODIGO_INICIO_MENSAJE ^ CODIGO_ENCENDIDO ^ comandoEncendidoRecibido == checksum)
+                if(CODIGO_INICIO_MENSAJE ^ CODIGO_ENCENDIDO ^ comandoEncendidoRecibido == checksum)
                 {
-                  if (comandoEncendidoRecibido == 1)
+                  if(comandoEncendidoRecibido == 1)
                   {
                     modoEjecucion = 'T';
                     digitalWrite(LED_ENCENDIDO, HIGH);
                   }
-                  if (comandoEncendidoRecibido == 0)
+                  if(comandoEncendidoRecibido == 0)
                   {
                     modoEjecucion = '_';
                     anguloDeseadoYPR[1] = 0;
@@ -561,28 +623,28 @@ void RecibirComando()
                     delay(1);
                     if (CODIGO_INICIO_MENSAJE ^ CODIGO_MOVIMIENTO ^ comandoPitch ^ comandoRoll ^ comandoAltura == checksum)
                     {
-                      anguloDeseadoYPR[1] = comandoPitch - 20;
-                      anguloDeseadoYPR[2] = comandoRoll - 20;
-                      if (comandoAltura == '+')
+                      anguloDeseadoYPR[1] = comandoPitch - MAXIMO_ANGULO_COMANDO;
+                      anguloDeseadoYPR[2] = comandoRoll - MAXIMO_ANGULO_COMANDO;
+                      if(comandoAltura == '+')
                       {
-                        if (alturaDeseada + INCREMENTO_ALTURA_COMANDO <= ALTURA_MAXIMA)
+                        if(alturaDeseada + INCREMENTO_ALTURA_COMANDO <= ALTURA_MAXIMA)
                         {
                           alturaDeseada = alturaDeseada + INCREMENTO_ALTURA_COMANDO;
                         }
                       }
-                      else if (comandoAltura == '-')
+                      else if(comandoAltura == '-')
                       {
-                        if (alturaDeseada - INCREMENTO_ALTURA_COMANDO >= 0)
+                        if(alturaDeseada - INCREMENTO_ALTURA_COMANDO >=0)
                         {
                           alturaDeseada = alturaDeseada - INCREMENTO_ALTURA_COMANDO;
-                        }
+                        }                        
                       }
                     }
-                  }
+                  } 
                 }
               }
             }
-          }
+          }            
         }
       }
     }
@@ -595,7 +657,7 @@ void EnviarAcknowledge(unsigned char codigoMensaje)
   ack[0] = CODIGO_INICIO_MENSAJE;
   ack[1] = CODIGO_ACK;
   ack[2] = codigoMensaje;
-  ack[3] = (ack[0] ^ ack[1] ^ ack[2]);
+  ack[3] =(ack[0] ^ ack[1] ^ ack[2]);
   Serial.write(ack, 4);
 }
 
@@ -637,7 +699,6 @@ void RecibirComandoASCII()
       if (Serial.available() > 0)
       {
         checksumRecibido_low = Serial.read();
-
         checksumRecibido_high = Serial.read();
         checksumRecibido = checksumRecibido_high * 256 + checksumRecibido_low;
       }
@@ -659,7 +720,9 @@ void RecibirComandoASCII()
       }
     }
   }
+
 }
+
 
 void ImprimirEstado()
 {
