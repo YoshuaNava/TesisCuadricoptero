b6c485fd198edb0b3324ffc63e5ca8ad7c2961aa
Yoshua_ROSIndigo <yoshua.nava.chocron@gmail.com>
2014-10-07 15:26:30 -0430

    Scripts para realizar análisis en frecuencia de la IMU.

--- Documento/CaracterizacionGiroscopio/AnalisisFrecuencia.m
+++ Documento/CaracterizacionGiroscopio/AnalisisFrecuencia.m
@@ -1,4 +1,4 @@
-function AnalisisFrecuencia(nombreArchivoCSV, frecuenciaMuestreo)
+function [pitch roll yaw] = AnalisisFrecuencia(nombreArchivoCSV, frecuenciaMuestreo)
     datosArchivo = csvread(nombreArchivoCSV, 1, 0);
     numeroDatos = size(datosArchivo,1);
     velocidadPitch = datosArchivo(:,5);
@@ -7,7 +7,6 @@ function AnalisisFrecuencia(nombreArchivoCSV, frecuenciaMuestreo)
     fft_velocidadPitch = fft(velocidadPitch, numeroDatos);
     fft_velocidadRoll = fft(velocidadRoll, numeroDatos);
     fft_velocidadYaw = fft(velocidadYaw, numeroDatos);
-    
     
     fft_velocidadPitch = fftshift(fft_velocidadPitch, numeroDatos);
     fft_velocidadRoll = fftshift(fft_velocidadRoll, numeroDatos);
@@ -20,7 +19,6 @@ function AnalisisFrecuencia(nombreArchivoCSV, frecuenciaMuestreo)
     shift_amplitud_fft_velocidadRoll = amplitud_fft_velocidadRoll / longitud_fft;
     shift_amplitud_fft_velocidadYaw = amplitud_fft_velocidadYaw / longitud_fft;
     maximoValor = max([max(shift_amplitud_fft_velocidadPitch), max(shift_amplitud_fft_velocidadRoll), max(shift_amplitud_fft_velocidadYaw)]);
-    %maximoValor = 2
     
     f=(-longitud_fft/2 : (longitud_fft/2-1)) * frecuenciaMuestreo/longitud_fft;
     figure('name',nombreArchivoCSV)
@@ -49,45 +47,49 @@ function AnalisisFrecuencia(nombreArchivoCSV, frecuenciaMuestreo)
     else
         ylim([0 1])
     end
-    title('Yaw')    
+    title('Yaw')
+    
+    pitch = shift_amplitud_fft_velocidadPitch;
+    roll = shift_amplitud_fft_velocidadRoll;
+    yaw = shift_amplitud_fft_velocidadYaw; 
 
-    frecuencia = (0 : numeroDatos - 1) * ( frecuenciaMuestreo / numeroDatos);
-    potencias_velocidadPitch = shift_amplitud_fft_velocidadPitch.*conj(shift_amplitud_fft_velocidadPitch)/numeroDatos;
-    potencias_velocidadRoll = shift_amplitud_fft_velocidadRoll.*conj(shift_amplitud_fft_velocidadRoll)/numeroDatos;
-    potencias_velocidadYaw = shift_amplitud_fft_velocidadYaw.*conj(shift_amplitud_fft_velocidadYaw)/numeroDatos;
-    maximoValor = max([max(potencias_velocidadPitch), max(potencias_velocidadRoll), max(potencias_velocidadYaw)]);
-    figure('name',nombreArchivoCSV)
-    plot(f, potencias_velocidadPitch)
-    xlabel('Frequency (Hz)')
-    ylabel('Power')
-    title('{\bf Periodogram Pitch}')
-    xlim([0 frecuenciaMuestreo/2])
-    if(maximoValor > 0)
-        ylim([0 maximoValor])
-    else
-        ylim([0 1])
-    end
-    figure('name',nombreArchivoCSV)
-    plot(f, potencias_velocidadRoll)
-    xlabel('Frequency (Hz)')
-    ylabel('Power')
-    title('{\bf Periodogram Roll}')
-    xlim([0 frecuenciaMuestreo/2])
-    if(maximoValor > 0)
-        ylim([0 maximoValor])
-    else
-        ylim([0 1])
-    end    
-    figure('name',nombreArchivoCSV)
-    plot(frecuencia, potencias_velocidadYaw)
-    xlabel('Frequency (Hz)')
-    ylabel('Power')
-    title('{\bf Periodogram Yaw}')
-    xlim([0 frecuenciaMuestreo/2])
-    if(maximoValor > 0)
-        ylim([0 maximoValor])
-    else
-        ylim([0 1])
-    end
+%     frecuencia = (0 : numeroDatos - 1) * ( frecuenciaMuestreo / numeroDatos);
+%     potencias_velocidadPitch = shift_amplitud_fft_velocidadPitch.*conj(shift_amplitud_fft_velocidadPitch)/numeroDatos;
+%     potencias_velocidadRoll = shift_amplitud_fft_velocidadRoll.*conj(shift_amplitud_fft_velocidadRoll)/numeroDatos;
+%     potencias_velocidadYaw = shift_amplitud_fft_velocidadYaw.*conj(shift_amplitud_fft_velocidadYaw)/numeroDatos;
+%     maximoValor = max([max(potencias_velocidadPitch), max(potencias_velocidadRoll), max(potencias_velocidadYaw)]);
+%     figure('name',nombreArchivoCSV)
+%     plot(f, potencias_velocidadPitch)
+%     xlabel('Frequency (Hz)')
+%     ylabel('Power')
+%     title('{\bf Periodogram Pitch}')
+%     xlim([0 frecuenciaMuestreo/2])
+%     if(maximoValor > 0)
+%         ylim([0 maximoValor])
+%     else
+%         ylim([0 1])
+%     end
+%     figure('name',nombreArchivoCSV)
+%     plot(f, potencias_velocidadRoll)
+%     xlabel('Frequency (Hz)')
+%     ylabel('Power')
+%     title('{\bf Periodogram Roll}')
+%     xlim([0 frecuenciaMuestreo/2])
+%     if(maximoValor > 0)
+%         ylim([0 maximoValor])
+%     else
+%         ylim([0 1])
+%     end    
+%     figure('name',nombreArchivoCSV)
+%     plot(frecuencia, potencias_velocidadYaw)
+%     xlabel('Frequency (Hz)')
+%     ylabel('Power')
+%     title('{\bf Periodogram Yaw}')
+%     xlim([0 frecuenciaMuestreo/2])
+%     if(maximoValor > 0)
+%         ylim([0 maximoValor])
+%     else
+%         ylim([0 1])
+%     end
 
 end
