70358d98e3c3f1e892e53242baa5f108b7ae031f
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2014-10-18 22:26:18 -0430

    Simulación de PID de velocidad angular.

--- Modelo físico/LazoAbierto_Kivrak.m
+++ Modelo físico/LazoAbierto_Kivrak.m
@@ -1,4 +1,4 @@
-%Modelo_Kivrak_Param()
+Modelo_Kivrak_Param()
 %Respuesta en lazo abierto a condiciones iniciales distintas de cero.
 t = 0:0.01:2;
 u = [zeros(size(t)); zeros(size(t)); zeros(size(t)); zeros(size(t))];
@@ -13,25 +13,3 @@ axis([0 2 -20 20])
 title('Respuesta en lazo abierto a condiciones iniciales distintas de cero.')
 xlabel('Tiempo (s)')
 ylabel('Posicion del cuadricoptero (m)')
-
-%%Simualacion
-%ControllabilityObservabillity_Kivrak()
-
-tF = 10;
-dt = 0.01;
-t = 0:dt:tF;
-numIteraciones = tF/dt;
-numEstados = 6;
-numEntradasControl = 4;
-u = zeros(numEntradasControl, numIteraciones);
-x_dot = zeros(numEstados, numIteraciones);
-x = zeros(numEstados, numIteraciones);
-x(1,1:numEstados) = [0 0 0 0 0 0];
-
-for i = 1:numIteraciones
-    x_dot(1:numEstados, i) = A*x(1:numEstados, i) + B*u(1:numEntradasControl, i);
-    x(1:numEstados,i+1) = x(1:numEstados, i) + dt*x_dot(1:numEstados, i);
-end
-
-plot(t,x(3:6,:))
-axis([0 2 -20 20])
