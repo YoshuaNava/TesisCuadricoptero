dee980672110fb030cb4542c6f57d37bac9c679b
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2014-11-26 15:51:43 -0430

    Documento: Faltan pruebas del sensor de altura y más nada.

--- Documento/CaracterizacionSensoresPID/AnalisisSistemasControl.m
+++ Documento/CaracterizacionSensoresPID/AnalisisSistemasControl.m
@@ -1,17 +1,35 @@
 function AnalisisSistemasControl(nombreArchivoCSV)
     datosArchivo = csvread(nombreArchivoCSV, 1, 0);
     numeroDatos = size(datosArchivo,1);
-    anguloYaw = datosArchivo(:,2);
-    anguloPitch = datosArchivo(:,3);
-    anguloRoll = datosArchivo(:,4);
-    velocidadYaw = datosArchivo(:,5);
-    velocidadPitch = datosArchivo(:,6);
-    velocidadRoll = datosArchivo(:,7);
+    anguloPitch = datosArchivo(:,2);
+    anguloRoll = datosArchivo(:,3);
+    anguloYaw = datosArchivo(:,4);
+    velocidadPitch = datosArchivo(700:900,5);
+    velocidadRoll = datosArchivo(100:300,6);
+    velocidadYaw = datosArchivo(300:500,7);
+    
+    x = 0:0.1:200;
+    y = 0*x;
     
     figure()
     plot(velocidadYaw);
+    hold;
+    plot(x,y,'k');
+    xlabel('Tiempo (s)')
+    ylabel('V. angular (grados/s)')
+    xlim([0 200])
     figure()
     plot(velocidadPitch);
+    hold;
+    plot(x,y,'k');
+    xlabel('Tiempo (s)')
+    ylabel('V. angular (grados/s)')
+    xlim([0 200])
     figure()
     plot(velocidadRoll);
+    hold;
+    plot(x,y,'k');
+    xlabel('Tiempo (s)')
+    ylabel('V. angular (grados/s)')
+    xlim([0 200])
 end
