ddd3d764baa56b7444714d39ca4f4f29c9de66f9
Yoshua Nava <yoshua.nava.chocron@gmail.com>
2015-01-12 00:27:16 -0430

    Sólo faltan los números de las ecuaciones y mejorar la introducción.

--- Documento/CaracterizacionSensoresPID/AnalisisSistemasControl.m
+++ Documento/CaracterizacionSensoresPID/AnalisisSistemasControl.m
@@ -3,33 +3,59 @@ function AnalisisSistemasControl(nombreArchivoCSV)
     numeroDatos = size(datosArchivo,1);
     anguloPitch = datosArchivo(:,2);
     anguloRoll = datosArchivo(:,3);
-    anguloYaw = datosArchivo(:,4);
-    velocidadPitch = datosArchivo(700:900,5);
-    velocidadRoll = datosArchivo(100:300,6);
-    velocidadYaw = datosArchivo(300:500,7);
+    anguloYaw = datosArchivo(1500:2000,4);
+    velocidadPitch = datosArchivo(:,5);
+    velocidadRoll = datosArchivo(:,6);
+    velocidadYaw = datosArchivo(1000:1500,7);
     
-    x = 0:0.1:200;
+    num_datos_recogidos = size(datosArchivo,1);
+    
+    x = 0:0.1:num_datos_recogidos;
     y = 0*x;
     
-    figure()
+    
+    
+    
+    figure('name','Posicion angular en Yaw')
+    plot(anguloYaw);
+    hold;
+    plot(x,y,'k');
+    xlabel('Tiempo (s)')
+    ylabel('Angulo (grados)')
+    xlim([0 500])
+    figure('name','Posicion angular en Pitch')
+    plot(anguloPitch);
+    hold;
+    plot(x,y,'k');
+    xlabel('Tiempo (s)')
+    ylabel('Angulo (grados)')
+    xlim([0 num_datos_recogidos])
+    figure('name','Posicion angular en Roll')
+    plot(anguloRoll);
+    hold;
+    plot(x,y,'k');
+    xlabel('Tiempo (s)')
+    ylabel('Angulo (grados)')
+    xlim([0 num_datos_recogidos])
+    figure('name','Velocidad angular en Yaw')
     plot(velocidadYaw);
     hold;
     plot(x,y,'k');
     xlabel('Tiempo (s)')
     ylabel('V. angular (grados/s)')
-    xlim([0 200])
-    figure()
+    xlim([0 500])
+    figure('name','Velocidad angular en Pitch')
     plot(velocidadPitch);
     hold;
     plot(x,y,'k');
     xlabel('Tiempo (s)')
     ylabel('V. angular (grados/s)')
-    xlim([0 200])
-    figure()
+    xlim([0 num_datos_recogidos])
+    figure('name','Velocidad angular en Roll')
     plot(velocidadRoll);
     hold;
     plot(x,y,'k');
     xlabel('Tiempo (s)')
     ylabel('V. angular (grados/s)')
-    xlim([0 200])
+    xlim([0 num_datos_recogidos])
 end
